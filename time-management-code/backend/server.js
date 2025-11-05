213d26b9e7613a15e3ec2a946acb2cec912547b1
BennyToth <Be.Toth@Proton.me>
2025-11-05 21:10:11 +0100

    server "bereinigt"

--- time-management-code/backend/server.js
+++ time-management-code/backend/server.js
@@ -8,11 +8,6 @@ const app = express();
 // 3. Den Port definieren (eine "Türnummer", auf der der Server lauscht)
 const port = 3001; // 3000 ist oft von React belegt, daher 3001
 
-// 4. Eine simple Test-Route definieren
-// Wenn jemand die Haupt-URL (/) aufruft, antworte mit "Hallo Welt"
-app.get('/', (req, res) => {
-  res.send('Hallo Welt! Der Server läuft. Vorerst.');
-});
 
 // 5. Den Server starten und auf dem Port "lauschen"
 app.listen(port, () => {
