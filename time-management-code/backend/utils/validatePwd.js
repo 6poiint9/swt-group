6aef65fd947328782320bd3d242d80511f31717b
BennyToth <Be.Toth@Proton.me>
2025-11-11 10:53:18 +0100

    created password validation function

---
+++ time-management-code/backend/utils/validatePwd.js
@@ -0,0 +1,21 @@
+    const validatePassword = async(username, password) => {
+    const uCheck = validateUsername(username);
+    const pCheck = validatePassword(password);
+
+    if (!uCheck.ok || !pCheck.ok) {
+      return res.status(401).json({ success: false, message: 'invalid login' });
+    }
+
+    // Lookup user in DB
+    const user = await User.findByUsername(uCheck.value);
+    if (!user) return res.status(401).json({ success: false, message: 'invalid login' });
+
+    // Verify password
+    const valid = await argon2.verify(user.password, pCheck.value);
+    if (!valid) return res.status(401).json({ success: false, message: 'invalid login' });
+
+    }
+    
+   module.exports = {
+    validatePassword
+   };
